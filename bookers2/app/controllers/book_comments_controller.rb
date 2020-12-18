f06623edf70d3ba1025b2ee1b0c4339a1d86612c
yuya-nagasaka <nagasaka.yuya0928@gmail.com>
2020-12-18 01:03:10 +0000

    いいねの重複修正・リダイレクト先修正

--- bookers2/app/controllers/book_comments_controller.rb
+++ bookers2/app/controllers/book_comments_controller.rb
@@ -4,12 +4,12 @@ class BookCommentsController < ApplicationController
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = book.id
     comment.save
-    redirect_to book_path(book)
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
     BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_to book_path(params[:book_id])
+    redirect_back(fallback_location: root_path)
   end
   
   private
