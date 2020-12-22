b7310145d9e17939aa41ac0df42bc8332ad1e6ac
yuya-nagasaka <nagasaka.yuya0928@gmail.com>
2020-12-22 06:00:28 +0000

    いいね機能非同期通信

--- bookers2/app/assets/javascripts/application.js
+++ bookers2/app/assets/javascripts/application.js
@@ -18,4 +18,3 @@
 //= require turbolinks
 //= require_tree .
 //= require jquery
-
--- bookers2/app/controllers/favorites_controller.rb
+++ bookers2/app/controllers/favorites_controller.rb
@@ -1,31 +1,17 @@
 class FavoritesController < ApplicationController
   
   def create
-    unless current_user.favorites.include?(clicked_book)
-       favorite = current_user.favorites.new(book_id: clicked_book.id)
-       if favorite.save
-         redirect_back(fallback_location: root_path)
-       end
-    else
-       redirect_back(fallback_location: root_path)
-    end
+    @book=Book.find(params[:book_id])
+       favorite = current_user.favorites.new(book_id: @book.id)
+       favorite.save
   end
 
   def destroy
-    unless current_user.favorites.include?(clicked_book)
-      favorite = current_user.favorites.find_by(book_id: clicked_book.id)
-      if favorite.destroy
-        redirect_back(fallback_location: root_path)
-      end
-    else
-       redirect_back(fallback_location: root_path)
-    end
+    @book=Book.find(params[:book_id])
+      favorite = current_user.favorites.find_by(book_id: @book.id)
+      favorite.destroy
   end
   
   private
-
-  def clicked_book
-    Book.find(params[:book_id])
-  end
   
 end
